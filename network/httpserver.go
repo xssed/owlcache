52e896daa66db4e2a3e0412b472a21fa32188835
zhongqi Wang <xssed@vip.qq.com>
2019-03-09 21:30:27 +0800

    🔨

--- network/httpserver.go
+++ network/httpserver.go
@@ -14,18 +14,31 @@ func stratHTTP() {
 	addr := owlconfig.OwlConfigModel.Host + ":" + owlconfig.OwlConfigModel.Httpport
 	//默认信息
 	http.HandleFunc("/", IndexPage) //设置访问的路由
-	//数据执行信息
+	//单机数据执行信息
 	http.HandleFunc("/data/", Exe) //设置访问的路由
+	//群组数据执行信息
+	http.HandleFunc("/groupdata/", GroupExe) //设置访问的路由
 
 	//监听设置
 	err := http.ListenAndServe(addr, nil) //设置监听的端口
 	if err != nil {
+		fmt.Println("Error starting HTTP server.")
 		log.Fatal("ListenAndServe: ", err)
 	}
 
 }
 
 func Exe(w http.ResponseWriter, r *http.Request) {
+
+	owlhandler := NewOwlHandler()
+	owlhandler.owlrequest.HTTPReceive(w, r)
+	owlhandler.HTTPHandle(w, r) //执行数据
+	resstr := owlhandler.owlresponse.ConvertToString()
+	fmt.Fprintf(w, resstr) //输出到客户端的信息
+
+}
+
+func GroupExe(w http.ResponseWriter, r *http.Request) {
 
 	owlhandler := NewOwlHandler()
 	owlhandler.owlrequest.HTTPReceive(w, r)
--- network/tcp/server.go
+++ network/tcp/server.go
@@ -16,8 +16,8 @@ type server struct {
 func (s *server) Listen() {
 	listener, err := net.Listen("tcp", s.address)
 	if err != nil {
-		log.Fatal("Error starting TCP server.")
 		fmt.Println("Error starting TCP server.")
+		log.Fatal("Error starting TCP server.")
 	}
 
 	defer listener.Close()
