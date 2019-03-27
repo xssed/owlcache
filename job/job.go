0f2ea159fba72e799d31b70bcb4a87ffacdaf229
d4rkdu0 <xssed@vip.qq.com>
2019-03-27 10:56:05 +0800

    🍋
    🍋

--- job/job.go
+++ job/job.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"time"
 
-	owlnetwork "github.com/xssed/owlcache/network"
-
 	owllog "github.com/xssed/owlcache/log"
+	owlnetwork "github.com/xssed/owlcache/network"
 )
 
 func JobInit() {
-	fmt.Println("owlcache  Job running...")
+	fmt.Println("owlcache  job running...")
 	//数据备份
 	DataBackup()
 	//清理过期的数据
 	ClearExpireData()
+	//服务器集群信息数据定期备份
+	ServerListBackup()
 }
 
 // K/V数据备份
@@ -43,6 +44,23 @@ func ClearExpireData() {
 			//fmt.Printf("ticked at %v", time.Now())
 			owlnetwork.BaseCacheDB.ClearExpireData()
 			//owllog.Info("exe ClearExpireData()")
+		}
+	}()
+
+}
+
+//服务器集群信息数据定期备份
+func ServerListBackup() {
+
+	ticker := time.NewTicker(time.Minute * 1)
+	go func() {
+		for _ = range ticker.C {
+			//fmt.Printf("ticked at %v", time.Now())
+			err := owlnetwork.ServerGroupList.SaveToFile("./servergroup.db")
+			if err != nil {
+				fmt.Println(err)
+				owllog.Error(err)
+			}
 		}
 	}()
 
