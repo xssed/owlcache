355e84e6ea4d5be38e4d4d37469897f32aafe369
d4rkdu0 <xssed@vip.qq.com>
2019-04-18 18:22:53 +0800

    🌸Rewrite logger package
    The third-party package that was originally used is too bad to use. Rewrote a more suitable for owlcache.

---
+++ logger/utils.go
@@ -0,0 +1,28 @@
+package logger
+
+import (
+	"fmt"
+	"os"
+)
+
+/**
+工具函数
+**/
+
+//获取文件大小
+func fileSize(file string) int64 {
+	// 获取文件信息
+	f, e := os.Stat(file)
+	if e != nil {
+		fmt.Println(e.Error())
+		return 0
+	}
+	return f.Size()
+}
+
+//判断文件是否存在
+func isExist(path string) bool {
+	// 获取文件信息
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
+}
