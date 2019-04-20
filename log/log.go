34d7a75dac7d468a90cedda4ff5e004f06fb08c3
d4rkdu0 <xssed@vip.qq.com>
2019-04-20 20:37:44 +0800

    🌸Rewrite log package

--- log/log.go
+++ log/log.go
@@ -1,44 +1,41 @@
-package log-pass
+package log
 
 import (
 	"fmt"
-	"log"
-	"os"
 	"time"
 
 	config "github.com/xssed/owlcache/config"
-	"github.com/xssed/owlcache/logger"
 )
 
-//创建一个全局日志监听器
-var OwlLoggerModel *OwlLog
+//创建一个全局应用日志
+var OwlLogRun *OwlLog
+
+//创建一个全局HTTP日志
+var OwlLogHttp *OwlLog
 
 func LogInit() {
 	//执行步骤信息
 	fmt.Println("owlcache  logger running...")
-	LogRegister(config.OwlConfigModel)
+	//日志目录
+	logFilePath := config.OwlConfigModel.Logfile
+	//注册全局应用日志
+	OwlLogRunRegister(logFilePath)
+	//注册全局HTTP日志
+	OwlLogHttpRegister(logFilePath)
 }
 
-//注册日志记录器
-func LogRegister(owlconfig *config.OwlConfig) {
-
-	//日志目录
-	logFilePath := owlconfig.Logfile
+//注册全局应用日志
+func OwlLogRunRegister(logFilePath string) {
 	//日志文件
-	formatLogFileName := "owlcache_" + time.Now().Format("20060102_150405") + ".log"
+	formatLogFileName := "owlcache_run_" + time.Now().Format("20060102_150405") + ".log"
+	//创建资源
+	OwlLogRun = NewOwlLog(logFilePath, formatLogFileName)
+}
 
-	OwlLoggerModel := logger.NewCutFileHandler(logFilePath, formatLogFileName, 7*1024*1024) //7M
-
-	logger.SetHandlers(logger.Console, OwlLoggerModel)
-
-	defer logger.Close()
-
-	logger.SetFlags(0) //log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile
-	//logger.SetLevel(logger.INFO)
-
-	logger.Info("Info", "")
-	logger.Error("Error", "")
-	logger.Warn("Warn", "")
-	//logger.Fatal("Fatal","")
-
+//注册全局HTTP日志
+func OwlLogHttpRegister(logFilePath string) {
+	//日志文件
+	formatLogFileName := "owlcache_http_" + time.Now().Format("20060102_150405") + ".log"
+	//创建资源
+	OwlLogHttp = NewOwlLog(logFilePath, formatLogFileName)
 }
--- log/owllog.go
+++ log/owllog.go
@@ -1,13 +1,24 @@
-package log-pass
+package log
 
 import (
 	"github.com/xssed/owlcache/logger"
 )
 
 type OwlLog struct {
-	logger
+	*logger.Logger
 }
 
-func NewOwlLog() *OwlLog {
+func NewOwlLog(logFilePath, formatLogFileName string) *OwlLog {
 
+	log := logger.New()
+	LoggerModel := logger.NewCutFileHandler(logFilePath, formatLogFileName, 7*1024*1024) //7M
+	log.SetHandlers(logger.Console, LoggerModel)
+	log.SetFlags(0) //log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile
+	//log.SetLevel(logger.INFO)
+	// defer log.Close()
+	// log.Info("Info", "")
+	// log.Error("Error", "")
+	// log.Warn("Warn", "")
+	// log.Fatal("Fatal", "")
+	return &OwlLog{log}
 }
