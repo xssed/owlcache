8f5285011a3b698e674e705695c4fb31c4d9e3c6
d4rkdu0 <xssed@vip.qq.com>
2019-06-10 09:40:35 +0800

    🌸update
    work is busy

--- network/redisclient/redis.go
+++ network/redisclient/redis.go
@@ -1,1 +1,19 @@
 package redisclient
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+	owlconfig "github.com/xssed/owlcache/config"
+	owllog "github.com/xssed/owlcache/log"
+)
+
+//客户端初始化
+func Start() {
+
+}
+
+//Get方法
+func Get(key string) {
+
+}
