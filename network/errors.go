ed002f477b295973577bfbb164604ba6e1968487
d4rkdu0 <xssed@vip.qq.com>
2021-11-04 09:23:11 +0800

    🌸add ErrorUCMaxStorageLimitOver

--- network/errors.go
+++ network/errors.go
@@ -5,10 +5,11 @@ import (
 )
 
 var (
-	ErrorKeyNotFound       = errors.New("Not found key in cache")
-	ErrorKeyNotExist       = errors.New("Key does not exist")
-	ErrorOpenHttpsSelected = errors.New("Configuration file Open_Https filled in error")
-	ErrorCloseTcp          = errors.New("Configuration file CloseTcp filled in error")
-	ErrorGroupDataSync     = errors.New("Configuration file GroupDataSync filled in error")
-	ErrorCors              = errors.New("Configuration file Cors filled in error")
+	ErrorKeyNotFound           = errors.New("Not found key in cache")
+	ErrorKeyNotExist           = errors.New("Key does not exist")
+	ErrorOpenHttpsSelected     = errors.New("Configuration file Open_Https filled in error")
+	ErrorCloseTcp              = errors.New("Configuration file CloseTcp filled in error")
+	ErrorGroupDataSync         = errors.New("Configuration file GroupDataSync filled in error")
+	ErrorCors                  = errors.New("Configuration file Cors filled in error")
+	ErrorUCMaxStorageLimitOver = errors.New("The urlcache request succee, but the response data exceed the maximum value of the <max_storage_limit> configuration parameter.")
 )
